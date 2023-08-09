0ccf7ce80eaf006a2949006716c256c73add6ca5
fakhriyya rahimova <rahimovafakhriyya@gmail.com>
2023-08-10 01:44:56 +0200

    few changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Location from './components/Location/Location';
 import Navbar from './components/Navbar';
 import CitySelector from './components/Weather/CitySelector';
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState('location');
+
+  const toggleComponent = () => {
+    setActiveComponent(activeComponent === 'location' ? 'weather' : 'location');
+  };
+
   return (
     <Router>
-      <Route path="/location" element={<Location />} />
       <Routes>
-
+        <Route path="/location" element={<Location />} />
         <Route path="/weather" element={<CitySelector />} />
       </Routes>
-      <Navbar />
+      <Navbar onClick={toggleComponent} />
     </Router>
   );
 }
